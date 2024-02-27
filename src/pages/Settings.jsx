62c43ed143e9f11288d554a60ae514b83a5ae61b
umutplvn <umutpehlivan2078@gmail.com>
2024-02-27 20:33:47 +0000

    up

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,91 +1,49 @@
-import * as React from "react";
-import { styled } from "@mui/material/styles";
-import MuiAccordion from "@mui/material/Accordion";
-import MuiAccordionSummary from "@mui/material/AccordionSummary";
-import MuiAccordionDetails from "@mui/material/AccordionDetails";
-import Typography from "@mui/material/Typography";
-import Footer from "../components/Footer";
-import { Box } from "@mui/material";
-import StarRoundedIcon from '@mui/icons-material/StarRounded';
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionActions from '@mui/material/AccordionActions';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Button from '@mui/material/Button';
+import Footer from '../components/Footer';
+import { Box, Typography } from '@mui/material';
 
-const Accordion = styled((props) => (
-  <MuiAccordion disableGutters elevation={0} square {...props} />
-))(({ theme }) => ({
-  borderTopLeftRadius:"0.5rem",
-  borderTopRightRadius:"0.5rem",
-  border: `${theme.palette.divider}`,
-  "&:not(:last-child)": {
-    borderBottom: 0,
-  },
-  "&::before": {
-    display: "none",
-  },
-}));
 
-const AccordionSummary = styled((props) => (
-  <MuiAccordionSummary
-    expandIcon={<StarRoundedIcon sx={{ fontSize: "1.5rem", backgroundColor:"#F7BE25", color:"white", borderRadius:"0.3rem",  }} />}
-    {...props}
-  />
-))(({ theme }) => ({
- 
-  backgroundColor:
-    theme.palette.mode === "dark"
-      ? "rgba(255, 255, 255, .05)"
-      : "rgba(0, 0, 0, .03)",
-  flexDirection: "row-reverse",
-  "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
-    transform: "rotate(90deg)",
-    transition:"0.5s"
-  },
-  "& .MuiAccordionSummary-content": {
-    marginLeft: theme.spacing(1)}
-}));
-
-const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
-  padding: theme.spacing(2),
-  borderTop: "1px solid rgba(0, 0, 0, .125)",
-}));
-
-export default function CustomizedAccordions() {
-  const [expanded, setExpanded] = React.useState("panel1");
-
-  const handleChange = (panel) => (event, newExpanded) => {
-    setExpanded(newExpanded ? panel : false);
-  };
-
+const Settings = () => {
   return (
-    <Box>
-          <Typography
-        sx={{ padding: "1rem 0.5rem", fontSize: "24px", fontWeight: "700", boxShadow:" rgba(17, 17, 26, 0.1) 0px 1px 0px ", backgroundColor:"#fdffff"
-      }}
+   <Box>
+         <Typography
+        sx={{
+          padding: "1rem 0.5rem",
+          fontSize: "24px",
+          fontWeight: "700",
+          boxShadow: " rgba(17, 17, 26, 0.1) 0px 1px 0px ",
+          backgroundColor: "#fdffff",
+          mb: "1rem",
+        }}
       >
         Settings
       </Typography>
-      <Box 
-      sx={{p:"1rem"}}
-      >
 
+    <Box>
 
-        <Accordion
-          expanded={expanded === "panel2"}
-          onChange={handleChange("panel2")}
-        >
-          <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-            <Typography>Starred Messages</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet,
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-
-     
-
-
-      </Box>
-      <Footer />
-    </Box>
-  );
+<Accordion>
+     <AccordionSummary
+       expandIcon={<ExpandMoreIcon />}
+       aria-controls="panel1-content"
+       id="panel1-header"
+     >
+       Accordion 1
+     </AccordionSummary>
+     <AccordionDetails>
+       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+       malesuada lacus ex, sit amet blandit leo lobortis eget.
+     </AccordionDetails>
+   </Accordion>
+   <Footer />
+ </Box>
+   </Box>
+  )
 }
+
+export default Settings
